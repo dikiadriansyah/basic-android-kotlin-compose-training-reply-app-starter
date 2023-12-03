1fc09fcd53af3a897ac67e1caf548ba4af3b0801
dikiadriansyah <diki725@gmail.com>
2023-12-03 13:24:35 +0700

    first commit

---
+++ app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,13 @@
+package com.example.reply.ui.utils
+
+enum class WindowStateUtils {
+    BOTTOM_NAVIGATION,
+    NAVIGATION_RAIL,
+    PERMANENT_NAVIGATION_DRAWER
+
+}
+
+
+enum class ReplyContentType{
+    LIST_ONLY, LIST_AND_DETAIL
+}
